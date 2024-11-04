97d3bd4464b18acff57f1d2dd35b2e8c46bc2018
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2024-11-04 00:52:00 -0300

    Add product_detail view to handle GET, PUT, and DELETE for individual products

--- backend/apps/products/api/views/products/product_list.py
+++ backend/apps/products/api/views/products/product_list.py
@@ -1,3 +1,5 @@
+from rest_framework.pagination import PageNumberPagination
+from ....pagination import ProductPagination  # Ajusta la ruta según la ubicación de ProductPagination
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -42,10 +44,14 @@ def product_list(request):
             products = products.filter(category_id=category_id)
         if type_id:
             products = products.filter(type_id=type_id)
+
+        # Paginar los productos usando ProductPagination
+        paginator = ProductPagination()
+        paginated_products = paginator.paginate_queryset(products, request)
         
-        # Serializar y devolver la lista de productos
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # Serializar y devolver la lista paginada de productos
+        serializer = ProductSerializer(paginated_products, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
@@ -57,7 +63,7 @@ def product_list(request):
             if 'technical_sheet_photo' in metadata:
                 try:
                     format, imgstr = metadata['technical_sheet_photo'].split(';base64,')
-                    ext = format.split('/')[-1]  # Obtener la extensión de archivo
+                    ext = format.split('/')[-1]
                     # Guardar la imagen decodificada en el campo `image`
                     product.image = ContentFile(base64.b64decode(imgstr), name=f"{product.name}_tech_sheet.{ext}")
                 except Exception as e:
--- backend/apps/products/pagination.py
+++ backend/apps/products/pagination.py
@@ -4,6 +4,6 @@ class ProductPagination(PageNumberPagination):
     """
     Custom pagination for product lists.
     """
-    page_size = 12  # Número de productos por página
+    page_size = 10  # Número de productos por página
     page_size_query_param = 'page_size'  # Permitir cambiar el tamaño de la página mediante un parámetro
     max_page_size = 100  # Tamaño máximo de página permitido
